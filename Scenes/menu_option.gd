93e2b53f5f3243c457928b54815096e5b52193e2
Shuflduf <shuflduf@gmail.com>
2024-12-15 18:30:00 -0700

    slightly better side menu

--- Scenes/menu_option.gd
+++ Scenes/menu_option.gd
@@ -1,16 +1,31 @@
 @tool
 class_name MenuOption
-extends HBoxContainer
+extends Control
+
+signal selected
 
 @export var item_name: String:
-	set(value):
-		item_name = value
-		$Label.text = value
+    set(value):
+        item_name = value
+        %Label.text = value
 
 func minimize(time: float):
-	var tween = get_tree().create_tween()
-	tween.tween_property($Label, ^"text", "", time)
+    var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+    tween.tween_property(%Label, ^"text", "", time)
 
 func expand(time: float):
-	var tween = get_tree().create_tween()
-	tween.tween_property($Label, ^"text", item_name, time)
+    var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+    tween.tween_property(%Label, ^"text", item_name, time)
+
+func deselect():
+    %Outline.self_modulate.a = 0
+
+func select():
+    %Outline.self_modulate.a = 1
+
+
+func _on_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        if event.is_pressed():
+            if event.button_index == MOUSE_BUTTON_LEFT:
+                selected.emit()
--- Scripts/menu_option_list.gd
+++ Scripts/menu_option_list.gd
@@ -1,25 +1,44 @@
 extends PanelContainer
 
-const SIZE_CHANGE_SECONDS = 0.5
+@export var SIZE_CHANGE_SECONDS = 0.5
 
 var collapsed = false
 var changing_size = false
 
-func _on_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			if event.button_index == MOUSE_BUTTON_LEFT:
-				if !changing_size:
-					changing_size = true
+func _ready() -> void:
+    for i in $Options.get_children():
+        i.selected.connect(select_item.bind(i))
+        i.deselect()
 
-					for i in $Options.get_children():
-						if !collapsed:
-							buffer_reenable()
-							i.minimize(SIZE_CHANGE_SECONDS)
-						else:
-							buffer_reenable()
-							i.expand(SIZE_CHANGE_SECONDS)
-					collapsed = !collapsed
+func _on_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        if event.is_pressed():
+            if event.button_index == MOUSE_BUTTON_LEFT:
+                change_size_all()
+
+func change_size_all():
+    if !changing_size:
+        changing_size = true
+        for i in $Options.get_children():
+            if !collapsed:
+                buffer_reenable()
+                i.minimize(SIZE_CHANGE_SECONDS)
+            else:
+                buffer_reenable()
+                i.expand(SIZE_CHANGE_SECONDS)
+        collapsed = !collapsed
 
 func buffer_reenable():
-	get_tree().create_timer(SIZE_CHANGE_SECONDS).timeout.connect(func(): changing_size = false)
+    get_tree().create_timer(SIZE_CHANGE_SECONDS).timeout.connect(func(): changing_size = false)
+
+func select_item(item: MenuOption):
+    if collapsed:
+        change_size_all()
+        return
+    if changing_size:
+        return
+
+    for i in $Options.get_children():
+        i.deselect()
+    item.select()
+    change_size_all()
