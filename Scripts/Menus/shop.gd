49e3f8e2d8aac17067c7c67ee6928ad305210c96
Shuflduf <shuflduf@gmail.com>
2024-12-21 14:11:23 -0700

    more progress towards working orders

--- Scripts/Menus/shop.gd
+++ Scripts/Menus/shop.gd
@@ -4,7 +4,7 @@ extends DefaultMenu
 
 func _ready() -> void:
 	for i in main_game.stocks_list.stocks:
-		%StockOptions.add_item(i.name)
+		%StockOptions.add_item(i.abbreviation)
 
 
 func _on_place_order_pressed() -> void:
@@ -12,4 +12,6 @@ func _on_place_order_pressed() -> void:
 	%VBoxContainer.add_child(new_trade)
 	new_trade.stock_name = %StockOptions.get_item_text(%StockOptions.get_selected_id())
 	new_trade.shares_amount = int(%SharesAmount.value)
+	new_trade.original_price = main_game.stocks_list.stocks.filter(func(stock): return new_trade.stock_name == stock.abbreviation)[0].price
+	new_trade.current_price = new_trade.original_price
 	new_trade.call_deferred(&"_update_label")
--- Scripts/stock.gd
+++ Scripts/stock.gd
@@ -14,14 +14,14 @@ extends Resource
 var _set_price = false
 ## Starting price of one share. This value will change, and is only set here as a starting point.
 @export var price: float:
-    set(value):
-        price = value
-        if !_set_price:
-            initial_price = value
-            history[0] = value
-            _set_price = true
-        else:
-            history.append(value)
+	set(value):
+		price = value
+		if !_set_price:
+			initial_price = value
+			history[0] = value
+			_set_price = true
+		else:
+			history.append(value)
 
 # Used to make graphs and stuff, each element is 20s apart
 var history: Array[float] = [0.0]
