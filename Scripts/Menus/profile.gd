b1b94ac9776df65b64bc59cf2b619549cc5539a4
Shuflduf <shuflduf@gmail.com>
2024-12-22 12:50:23 -0700

    update profile

--- Scripts/Menus/profile.gd
+++ Scripts/Menus/profile.gd
@@ -1,7 +1,8 @@
 extends DefaultMenu
 
 func _ready() -> void:
+	tick()
 	main_game.stocks_updated.connect(tick)
 
 func tick():
-	%Money.text = "Money: $" + str(main_game.money)
+	%Money.text = "Money: $" + str(main_game.money).pad_decimals(2)
